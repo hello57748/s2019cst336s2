d834ff150f833d75227ed0e1a253925b5f98add8
Samuel Peters <sampeters@csumb.edu>
2019-02-14 07:48:22 +0000

    lab 3 done

--- Labs/Lab2/js/functions.js
+++ Labs/Lab2/js/functions.js
@@ -2,21 +2,20 @@ var randomNumber = Math.floor(Math.random() * 99) + 1;
 var guesses = document.querySelector('#guesses');
 var lastResult = document.querySelector('#lastResult');
 var lowOrHigh = document.querySelector('#lowOrHigh');
-            
 var guessSubmit = document.querySelector('.guessSubmit');
 var guessField = document.querySelector('.guessField');
-            
-var guessCount = 1;
 var resetButton = document.querySelector('#reset');
-guessField.focus();
-resetButton.style.display = 'none';
-            
-var winsLosses = document.querySelector('#winsLosses');
+var guessCount = 1;
 var numberOfWins = 0;
 var numberOfLosses = 0;
+var winsLosses = document.querySelector('#winsLosses');
+var resetButton;
+
+
+guessField.focus();
+resetButton.style.display = 'none';
 winsLosses.innerHTML = 'Wins: ' + numberOfWins + ' Losses: ' + numberOfLosses;
             
-var resetButton;
 function checkGuess() {
     var userGuess = Number(guessField.value);
     if (userGuess < 0 || userGuess > 99 || !Number.isInteger(userGuess)) {
@@ -93,7 +92,3 @@ function resetGame() {
     randomNumber = Math.floor(Math.random() * 99) + 1;
     winsLosses.innerHTML = 'Wins: ' + numberOfWins + ' Losses: ' + numberOfLosses;
 }
-            
-            
-// console.log(randomNumber);
-// document.getElementById("numberToGuess").innerHTML = randomNumber;
