9c5af5fd921efa7de94a27b0f5af5ca5dcfa5f0e
Samuel Peters <sampeters@csumb.edu>
2019-04-06 21:58:10 +0000

    lab 7 done

--- Labs/Lab7/dashboard/login.php
+++ Labs/Lab7/dashboard/login.php
@@ -36,6 +36,13 @@
       $dbname = 'heroku_d727c510ebe6dad';
   
       // Get Data from DB
+      if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+          $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+          $host = $url["host"];
+          $dbname = substr($url["path"], 1);
+          $username = $url["user"];
+          $password = $url["pass"];
+      } 
       $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
       $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 
