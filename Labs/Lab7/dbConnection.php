c0613cf96812c8dd530b5d78bf63d903ec8d4039
Samuel Peters <sampeters@csumb.edu>
2019-04-06 21:34:02 +0000

    lab 7 done

--- Labs/Lab7/dbConnection.php
+++ Labs/Lab7/dbConnection.php
@@ -2,22 +2,22 @@
 
 // heroku_d727c510ebe6dad
 // ottermart
-function getDatabaseConnection($dbname = 'ottermart') {
-    // $host = 'us-cdbr-iron-east-03.cleardb.net'; //cloud 9 acting as host
-    // $username = 'b8282773fb41e0';
-    // $password = 'a78ad875';
+function getDatabaseConnection($dbname = 'heroku_d727c510ebe6dad') {
+    $host = 'us-cdbr-iron-east-03.cleardb.net'; //cloud 9 acting as host
+    $username = 'b8282773fb41e0';
+    $password = 'a78ad875';
     
-    $host = 'localhost'; //cloud 9 acting as host
-    $username = 'hello57748';
-    $password = '';
+    // $host = 'localhost'; //cloud 9 acting as host
+    // $username = 'hello57748';
+    // $password = '';
     
-    // if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
-    //     $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    //     $host = $url["host"];
-    //     $dbname = substr($url["path"], 1);
-    //     $username = $url["user"];
-    //     $password = $url["pass"];
-    // } 
+    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $host = $url["host"];
+        $dbname = substr($url["path"], 1);
+        $username = $url["user"];
+        $password = $url["pass"];
+    } 
 
     //creates db connection
     $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
