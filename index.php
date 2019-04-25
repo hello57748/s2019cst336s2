3005c8c87654e8f0d1c777868a396c041b3a6fa6
Samuel Peters <sampeters@csumb.edu>
2019-04-25 20:04:08 +0000

    link to project 5 from main index.php added

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+$log = new Monolog\Logger('monolog-test');
+$log->pushHandler(new Monolog\Handler\StreamHandler(__DIR__ . '/app.log', Monolog\Logger::INFO));
+$log->info('I am inside the monolog test page');
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+        Hello World!<br>
+        <a href="http://hello57748-s2019cst336s2.herokuapp.com/Labs/Lab4/main.html">Lab 4</a><br>
+        <a href="http://hello57748-s2019cst336s2.herokuapp.com/Labs/Lab7/dashboard/login.html">Lab 7</a><br>
+        <a href="http://hello57748-s2019cst336s2.herokuapp.com/Labs/Lab8/index.html">Lab 8</a><br>
+        <a href="http://hello57748-s2019cst336s2.herokuapp.com/Assignments/project5">Project 5</a><br>
+        
+    </body>
+</html>
