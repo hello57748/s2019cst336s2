3f01809a4f7635cc52472cf296876fa6df2acb14
Samuel Peters <sampeters@csumb.edu>
2019-04-25 20:06:05 +0000

    link to project 5 from main index.php added

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $log->info('I am inside the monolog test page');
         <a href="http://hello57748-s2019cst336s2.herokuapp.com/Labs/Lab4/main.html">Lab 4</a><br>
         <a href="http://hello57748-s2019cst336s2.herokuapp.com/Labs/Lab7/dashboard/login.html">Lab 7</a><br>
         <a href="http://hello57748-s2019cst336s2.herokuapp.com/Labs/Lab8/index.html">Lab 8</a><br>
-        <a href="http://hello57748-s2019cst336s2.herokuapp.com/Assignments/project5">Project 5</a><br>
+        <a href="http://hello57748-s2019cst336s2.herokuapp.com/Assignments/project5/index.php">Project 5</a><br>
         
     </body>
 </html>
