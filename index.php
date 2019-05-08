c040a32c928504f2114042eb346ed6fdfe7ea5ed
Samuel Peters <sampeters@csumb.edu>
2019-05-08 00:17:00 +0000

    new index file

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         <a href="http://hello57748-s2019cst336s2.herokuapp.com/Labs/Lab7/dashboard/login.html">Lab 7</a><br>
         <a href="http://hello57748-s2019cst336s2.herokuapp.com/Labs/Lab8/index.html">Lab 8</a><br>
         <a href="http://hello57748-s2019cst336s2.herokuapp.com/Assignments/project5/index.php">Project 5</a><br>
-        <a href="http://hello57748-s2019cst336s2.herokuapp.com/Assignments/project5/index.php">Final Project: Right Review</a><br>
+        <a href="https://hello57748-s2019cst336s2.herokuapp.com/final_project/right_review_codebase/index.php">Final Project: Right Review</a><br>
         
     </body>
 </html>
