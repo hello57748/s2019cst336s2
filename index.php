02c8f1e76d622fd273c5c97b3e333bf5dbc7c7c2
Samuel Peters <sampeters@csumb.edu>
2019-05-08 00:15:01 +0000

    new index file

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
+// <?php
+// require __DIR__ . '/vendor/autoload.php';
 
-$log = new Monolog\Logger('monolog-test');
-$log->pushHandler(new Monolog\Handler\StreamHandler(__DIR__ . '/app.log', Monolog\Logger::INFO));
-$log->info('I am inside the monolog test page');
-?>
+// $log = new Monolog\Logger('monolog-test');
+// $log->pushHandler(new Monolog\Handler\StreamHandler(__DIR__ . '/app.log', Monolog\Logger::INFO));
+// $log->info('I am inside the monolog test page');
+// ?>
 
 <!DOCTYPE html>
 <html>
@@ -16,7 +16,8 @@ $log->info('I am inside the monolog test page');
         <a href="http://hello57748-s2019cst336s2.herokuapp.com/Labs/Lab4/main.html">Lab 4</a><br>
         <a href="http://hello57748-s2019cst336s2.herokuapp.com/Labs/Lab7/dashboard/login.html">Lab 7</a><br>
         <a href="http://hello57748-s2019cst336s2.herokuapp.com/Labs/Lab8/index.html">Lab 8</a><br>
-        <!--<a href="http://hello57748-s2019cst336s2.herokuapp.com/Assignments/project5/index.php">Project 5</a><br>-->
+        <a href="http://hello57748-s2019cst336s2.herokuapp.com/Assignments/project5/index.php">Project 5</a><br>
+        <a href="http://hello57748-s2019cst336s2.herokuapp.com/Assignments/project5/index.php">Final Project: Right Review</a><br>
         
     </body>
 </html>
